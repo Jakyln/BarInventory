97703a771fba01491564016c3d16049c1690c3e8
voxars <voxarsa@gmail.com>
2022-07-21 10:25:05 +0200

    Form v1 design terminer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,273 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPlus1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnMoins1 = new System.Windows.Forms.Button();
+            this.btnMoins2 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btnPlus2 = new System.Windows.Forms.Button();
+            this.btnMoins3 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btnPlus3 = new System.Windows.Forms.Button();
+            this.btnMoins4 = new System.Windows.Forms.Button();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.btnPlus4 = new System.Windows.Forms.Button();
+            this.btnMoins5 = new System.Windows.Forms.Button();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.btnPlus5 = new System.Windows.Forms.Button();
+            this.lblPrix5 = new System.Windows.Forms.Label();
+            this.lblPrix4 = new System.Windows.Forms.Label();
+            this.lblPrix3 = new System.Windows.Forms.Label();
+            this.lblPrix2 = new System.Windows.Forms.Label();
+            this.lblPrix1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlus1
+            // 
+            this.btnPlus1.Location = new System.Drawing.Point(461, 111);
+            this.btnPlus1.Name = "btnPlus1";
+            this.btnPlus1.Size = new System.Drawing.Size(33, 30);
+            this.btnPlus1.TabIndex = 2;
+            this.btnPlus1.Text = "+";
+            this.btnPlus1.UseVisualStyleBackColor = true;
+            this.btnPlus1.Click += new System.EventHandler(this.btnPlus1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(277, 120);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // btnMoins1
+            // 
+            this.btnMoins1.Location = new System.Drawing.Point(422, 111);
+            this.btnMoins1.Name = "btnMoins1";
+            this.btnMoins1.Size = new System.Drawing.Size(33, 30);
+            this.btnMoins1.TabIndex = 4;
+            this.btnMoins1.Text = "-";
+            this.btnMoins1.UseVisualStyleBackColor = true;
+            this.btnMoins1.Click += new System.EventHandler(this.btnMoins1_Click);
+            // 
+            // btnMoins2
+            // 
+            this.btnMoins2.Location = new System.Drawing.Point(422, 147);
+            this.btnMoins2.Name = "btnMoins2";
+            this.btnMoins2.Size = new System.Drawing.Size(33, 30);
+            this.btnMoins2.TabIndex = 7;
+            this.btnMoins2.Text = "-";
+            this.btnMoins2.UseVisualStyleBackColor = true;
+            this.btnMoins2.Click += new System.EventHandler(this.btnMoins2_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(277, 156);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 13);
+            this.lbl2.TabIndex = 6;
+            this.lbl2.Click += new System.EventHandler(this.lbl2_Click);
+            // 
+            // btnPlus2
+            // 
+            this.btnPlus2.Location = new System.Drawing.Point(461, 147);
+            this.btnPlus2.Name = "btnPlus2";
+            this.btnPlus2.Size = new System.Drawing.Size(33, 30);
+            this.btnPlus2.TabIndex = 5;
+            this.btnPlus2.Text = "+";
+            this.btnPlus2.UseVisualStyleBackColor = true;
+            this.btnPlus2.Click += new System.EventHandler(this.btnPlus2_Click);
+            // 
+            // btnMoins3
+            // 
+            this.btnMoins3.Location = new System.Drawing.Point(422, 183);
+            this.btnMoins3.Name = "btnMoins3";
+            this.btnMoins3.Size = new System.Drawing.Size(33, 30);
+            this.btnMoins3.TabIndex = 10;
+            this.btnMoins3.Text = "-";
+            this.btnMoins3.UseVisualStyleBackColor = true;
+            this.btnMoins3.Click += new System.EventHandler(this.btnMoins3_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(277, 192);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 13);
+            this.lbl3.TabIndex = 9;
+            this.lbl3.Click += new System.EventHandler(this.lbl3_Click);
+            // 
+            // btnPlus3
+            // 
+            this.btnPlus3.Location = new System.Drawing.Point(461, 183);
+            this.btnPlus3.Name = "btnPlus3";
+            this.btnPlus3.Size = new System.Drawing.Size(33, 30);
+            this.btnPlus3.TabIndex = 8;
+            this.btnPlus3.Text = "+";
+            this.btnPlus3.UseVisualStyleBackColor = true;
+            this.btnPlus3.Click += new System.EventHandler(this.btnPlus3_Click);
+            // 
+            // btnMoins4
+            // 
+            this.btnMoins4.Location = new System.Drawing.Point(422, 219);
+            this.btnMoins4.Name = "btnMoins4";
+            this.btnMoins4.Size = new System.Drawing.Size(33, 30);
+            this.btnMoins4.TabIndex = 13;
+            this.btnMoins4.Text = "-";
+            this.btnMoins4.UseVisualStyleBackColor = true;
+            this.btnMoins4.Click += new System.EventHandler(this.btnMoins4_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(277, 228);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 13);
+            this.lbl4.TabIndex = 12;
+            this.lbl4.Click += new System.EventHandler(this.lbl4_Click);
+            // 
+            // btnPlus4
+            // 
+            this.btnPlus4.Location = new System.Drawing.Point(461, 219);
+            this.btnPlus4.Name = "btnPlus4";
+            this.btnPlus4.Size = new System.Drawing.Size(33, 30);
+            this.btnPlus4.TabIndex = 11;
+            this.btnPlus4.Text = "+";
+            this.btnPlus4.UseVisualStyleBackColor = true;
+            this.btnPlus4.Click += new System.EventHandler(this.btnPlus4_Click);
+            // 
+            // btnMoins5
+            // 
+            this.btnMoins5.Location = new System.Drawing.Point(422, 255);
+            this.btnMoins5.Name = "btnMoins5";
+            this.btnMoins5.Size = new System.Drawing.Size(33, 30);
+            this.btnMoins5.TabIndex = 16;
+            this.btnMoins5.Text = "-";
+            this.btnMoins5.UseVisualStyleBackColor = true;
+            this.btnMoins5.Click += new System.EventHandler(this.btnMoins5_Click);
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(277, 264);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(0, 13);
+            this.lbl5.TabIndex = 15;
+            this.lbl5.Click += new System.EventHandler(this.lbl5_Click);
+            // 
+            // btnPlus5
+            // 
+            this.btnPlus5.Location = new System.Drawing.Point(461, 255);
+            this.btnPlus5.Name = "btnPlus5";
+            this.btnPlus5.Size = new System.Drawing.Size(33, 30);
+            this.btnPlus5.TabIndex = 14;
+            this.btnPlus5.Text = "+";
+            this.btnPlus5.UseVisualStyleBackColor = true;
+            this.btnPlus5.Click += new System.EventHandler(this.btnPlus5_Click);
+            // 
+            // lblPrix5
+            // 
+            this.lblPrix5.AutoSize = true;
+            this.lblPrix5.Location = new System.Drawing.Point(381, 264);
+            this.lblPrix5.Name = "lblPrix5";
+            this.lblPrix5.Size = new System.Drawing.Size(0, 13);
+            this.lblPrix5.TabIndex = 21;
+            this.lblPrix5.Click += new System.EventHandler(this.lblPrix5_Click);
+            // 
+            // lblPrix4
+            // 
+            this.lblPrix4.AutoSize = true;
+            this.lblPrix4.Location = new System.Drawing.Point(381, 228);
+            this.lblPrix4.Name = "lblPrix4";
+            this.lblPrix4.Size = new System.Drawing.Size(0, 13);
+            this.lblPrix4.TabIndex = 20;
+            this.lblPrix4.Click += new System.EventHandler(this.lblPrix4_Click);
+            // 
+            // lblPrix3
+            // 
+            this.lblPrix3.AutoSize = true;
+            this.lblPrix3.Location = new System.Drawing.Point(381, 192);
+            this.lblPrix3.Name = "lblPrix3";
+            this.lblPrix3.Size = new System.Drawing.Size(0, 13);
+            this.lblPrix3.TabIndex = 19;
+            this.lblPrix3.Click += new System.EventHandler(this.lblPrix3_Click);
+            // 
+            // lblPrix2
+            // 
+            this.lblPrix2.AutoSize = true;
+            this.lblPrix2.Location = new System.Drawing.Point(381, 156);
+            this.lblPrix2.Name = "lblPrix2";
+            this.lblPrix2.Size = new System.Drawing.Size(0, 13);
+            this.lblPrix2.TabIndex = 18;
+            this.lblPrix2.Click += new System.EventHandler(this.lblPrix2_Click);
+            // 
+            // lblPrix1
+            // 
+            this.lblPrix1.AutoSize = true;
+            this.lblPrix1.Location = new System.Drawing.Point(381, 120);
+            this.lblPrix1.Name = "lblPrix1";
+            this.lblPrix1.Size = new System.Drawing.Size(0, 13);
+            this.lblPrix1.TabIndex = 17;
+            this.lblPrix1.Click += new System.EventHandler(this.lblPrix1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblPrix5);
+            this.Controls.Add(this.lblPrix4);
+            this.Controls.Add(this.lblPrix3);
+            this.Controls.Add(this.lblPrix2);
+            this.Controls.Add(this.lblPrix1);
+            this.Controls.Add(this.btnMoins5);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.btnPlus5);
+            this.Controls.Add(this.btnMoins4);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.btnPlus4);
+            this.Controls.Add(this.btnMoins3);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.btnPlus3);
+            this.Controls.Add(this.btnMoins2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btnPlus2);
+            this.Controls.Add(this.btnMoins1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnPlus1);
+            this.Name = "Form1";
+            this.Text = "Inventaire du bar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPlus1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnMoins1;
+        private System.Windows.Forms.Button btnMoins2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btnPlus2;
+        private System.Windows.Forms.Button btnMoins3;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btnPlus3;
+        private System.Windows.Forms.Button btnMoins4;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Button btnPlus4;
+        private System.Windows.Forms.Button btnMoins5;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button btnPlus5;
+        private System.Windows.Forms.Label lblPrix5;
+        private System.Windows.Forms.Label lblPrix4;
+        private System.Windows.Forms.Label lblPrix3;
+        private System.Windows.Forms.Label lblPrix2;
+        private System.Windows.Forms.Label lblPrix1;
     }
 }
 
