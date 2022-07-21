c8a2133788af9ba9c9e6102d699f45c00c2559f4
Jakyln <kalaycijulien@gmail.com>
2022-07-21 10:21:07 +0200

    Création des classes Inventaire et Boisson

--- Program.cs
+++ Program.cs
@@ -6,6 +6,59 @@ using System.Windows.Forms;
 
 namespace BarInventory
 {
+    public class Boisson
+    {
+        int id;
+        string name;
+        double prixDeCreation;
+        double prixDeVente;
+
+        public Boisson()
+        {
+
+        }
+        public Boisson(string name, double prixDeCreation,double prixDeVente)
+        {
+            this.name = name;
+            this.prixDeCreation = prixDeCreation;
+            this.prixDeVente = prixDeVente;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double PrixDeCreation { get; set; }
+        public double PrixDeVente { get; set; }
+    }
+
+    public class Inventaire
+    {
+        List<Boisson> listBoissons;
+
+        public Inventaire()
+        {
+
+        }
+        public Inventaire(List<Boisson> listBoissons, double prixDeCreationTotal, double prixDeVenteTotal)
+        {
+            this.listBoissons = listBoissons;
+        }
+        public List<Boisson> ListBoissons { get; set; }
+
+        public int getStockTotal()
+        {
+            //TODO
+            return 0;
+        }
+        public double getPrixDeVenteTotal()
+        {
+            //TODO
+            return 0d;
+        }
+        public double getPrixDeCreationTotal()
+        {
+            //TODO
+            return 0d;
+        }
+    }
     internal static class Program
     {
         /// <summary>
@@ -18,5 +71,8 @@ namespace BarInventory
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
     }
+
+
 }
